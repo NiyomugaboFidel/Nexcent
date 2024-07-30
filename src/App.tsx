1538811414f6e62280996d0acee68ed64dc1b73c
developer <developer@gmail.com>
2024-07-30 15:39:35 +0200

    second commit,2rd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Community from "./components/Community/Community";
