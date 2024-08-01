b8531aa0dab914337f2a631e5703d9af194495c9
developer <developer@gmail.com>
2024-08-01 14:39:39 +0200

    6s

--- src/components/Body/Calender.tsx
+++ src/components/Body/Calender.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import image from "../../assets/home-2.svg";
+import  { ScrollReveal2 } from "../Animations/ScrollReveal";
+
 const Calender: React.FC = () => {
   return (
 <section>    
 <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8">
   <div className="md:w-11/12 mx-auto flex flex-col md:flex-row gap-10 justify-between items-center ">
     <div>
+      <ScrollReveal2 direction="left">
       <img src={image} alt="unlock" />
+      </ScrollReveal2>
     </div>
     <div className="md:w-4/5 mx-auto w-full">
+    
+    <ScrollReveal2 direction="right">
       <h2 className="text-xl md:text-2xl lg:text-3xl leading-snug line-clamp-2 text-dGrey font-semibold mb-4 md:w-4/5">
       How to design your site footer like we did
       </h2>
@@ -26,6 +32,7 @@ const Calender: React.FC = () => {
       <button className="btn-primary">
         Learn More
       </button>
+    </ScrollReveal2>
     </div>
   </div>
 
--- src/components/Body/Unlock.tsx
+++ src/components/Body/Unlock.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image from "../../assets/home-3.svg";
+import ScrollReveal, { ScrollReveal2 } from "../Animations/ScrollReveal";
 const Unlock: React.FC = () => {
   return (
     <section>
@@ -8,10 +9,13 @@ const Unlock: React.FC = () => {
       <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8">
         <div className="md:w-11/12 mx-auto flex flex-col md:flex-row gap-10 justify-between items-center ">
           <div>
+            <ScrollReveal2 direction="left">
             <img src={image} alt="unlock" />
+            </ScrollReveal2>
           </div>
           <div className="md:w-4/5 mx-auto w-full">
-            <h2 className="text-xl md:text-2xl lg:text-3xl leading-snug line-clamp-2 text-dGrey font-semibold mb-4 md:w-4/5">
+           <ScrollReveal>
+           <h2 className="text-xl md:text-2xl lg:text-3xl leading-snug line-clamp-2 text-dGrey font-semibold mb-4 md:w-4/5">
             The unseen of spending three years at Pixelgrade
             </h2>
             <p className="md:w-3/4  text-[14px] md:text-p text-grey mb-8 line-clamp-4 ">
@@ -25,6 +29,7 @@ const Unlock: React.FC = () => {
             <button className="btn-primary">
               Learn More
             </button>
+           </ScrollReveal>
           </div>
         </div>
 
